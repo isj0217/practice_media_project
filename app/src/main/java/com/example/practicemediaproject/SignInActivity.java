4f65d9fc01aab002ca6a5e3acf47c2b09a98f34f
조인성 <insung@joinseong-ui-MacBookPro.local>
2020-08-14 23:15:46 +0900

    SignUp 입력양식 적절하면 회원가입 완료, SignIn 이후 Main으로 이동 구현

--- app/src/main/java/com/example/practicemediaproject/SignInActivity.java
+++ app/src/main/java/com/example/practicemediaproject/SignInActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.practicemediaproject.main.MainActivity;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class SignInActivity extends AppCompatActivity {
     private EditText et_sign_in_pw;
     private Button btn_sign_in_sign_in;
     private Button btn_sign_in_find_id_or_pw;
+    private Button bnt_sign_in_sign_in;
     private Button btn_sign_in_sign_up;
 
     private Intent intent;
@@ -32,6 +36,33 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         bindViews();
+
+        btn_sign_in_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // todo before server connection
+                // 1. 아이디랑 비밀번호 EditText를 읽어와서 비어있지만 않다면 그냥 MainActivity로 이동
+
+                if (et_sign_in_id.getText().toString().equals("") && et_sign_in_pw.getText().toString().equals("")){
+                    Toast.makeText(SignInActivity.this, "Input ID and PASSWORD", Toast.LENGTH_SHORT).show();
+                } else if (et_sign_in_id.getText().toString().equals("")){
+                    Toast.makeText(SignInActivity.this, "Input ID", Toast.LENGTH_SHORT).show();
+                } else if (et_sign_in_pw.getText().toString().equals("")){
+                    Toast.makeText(SignInActivity.this, "Input PW", Toast.LENGTH_SHORT).show();
+                } else{
+                    intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    finish();
+                }
+
+
+                // todo after server connection
+                // 1. 사용자가 입력한 아이디랑 비밀번호를 edittext에서 읽어와서 서버로 두 정보를 보낸 후에
+                // 2. 올바르다는 응답이 오면 로그인 성공(MainActivity로 이동)
+                // 3. 틀리다는 응답이 오면 로그인 실패(팝업창 띄우기)
+            }
+        });
 
         btn_sign_in_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/practicemediaproject/SignUpActivity.java
+++ app/src/main/java/com/example/practicemediaproject/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.practicemediaproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,7 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.practicemediaproject.main.MainActivity;
@@ -27,6 +30,12 @@ public class SignUpActivity extends AppCompatActivity {
     private ImageView iv_sign_up_check_pw_again;
     private ImageView iv_sign_up_check_email;
     private ImageView iv_sign_up_check_nickname;
+
+    private boolean is_suitable_id;
+    private boolean is_suitable_pw;
+    private boolean is_suitable_pw_again;
+    private boolean is_suitable_email;
+    private boolean is_suitable_nickname;
 
     private Intent intent;
 
@@ -61,20 +70,20 @@ public class SignUpActivity extends AppCompatActivity {
         et_sign_up_id.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (editable.length() >= 4 && editable.length() <= 20){
+                if (editable.length() >= 4 && editable.length() <= 20) {
                     iv_sign_up_check_id.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_green);
-                }else{
+                    is_suitable_id = true;
+                } else {
                     iv_sign_up_check_id.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_red);
+                    is_suitable_id = false;
                 }
             }
         });
@@ -82,20 +91,20 @@ public class SignUpActivity extends AppCompatActivity {
         et_sign_up_pw.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (editable.length() >= 4 && editable.length() <= 20){
+                if (editable.length() >= 4 && editable.length() <= 20) {
                     iv_sign_up_check_pw.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_green);
-                }else{
+                    is_suitable_pw = true;
+                } else {
                     iv_sign_up_check_pw.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_red);
+                    is_suitable_pw = false;
                 }
             }
         });
@@ -103,20 +112,20 @@ public class SignUpActivity extends AppCompatActivity {
         et_sign_up_pw_again.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (editable.length() >= 4 && editable.length() <= 20 && editable.toString().equals(et_sign_up_pw.getText().toString())){
+                if (editable.length() >= 4 && editable.length() <= 20 && editable.toString().equals(et_sign_up_pw.getText().toString())) {
                     iv_sign_up_check_pw_again.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_green);
-                }else{
+                    is_suitable_pw_again = true;
+                } else {
                     iv_sign_up_check_pw_again.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_red);
+                    is_suitable_pw_again = false;
                 }
             }
         });
@@ -124,20 +133,20 @@ public class SignUpActivity extends AppCompatActivity {
         et_sign_up_email.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (editable.toString().matches(email_pattern) && editable.length() > 0){
+                if (editable.toString().matches(email_pattern) && editable.length() > 0) {
                     iv_sign_up_check_email.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_green);
-                }else{
+                    is_suitable_email = true;
+                } else {
                     iv_sign_up_check_email.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_red);
+                    is_suitable_email = false;
                 }
             }
         });
@@ -145,27 +154,27 @@ public class SignUpActivity extends AppCompatActivity {
         et_sign_up_nickname.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (editable.length() >= 4){
+                if (editable.length() >= 4) {
                     iv_sign_up_check_nickname.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_green);
-                }else{
+                    is_suitable_nickname = true;
+                } else {
                     iv_sign_up_check_nickname.setImageResource(R.drawable.ic_baseline_check_circle_outline_24_red);
+                    is_suitable_nickname = false;
                 }
             }
         });
     }
 
     public void customOnClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.iv_sign_up_go_back:
                 intent = new Intent(SignUpActivity.this, SignInActivity.class);
                 startActivity(intent);
@@ -174,11 +183,31 @@ public class SignUpActivity extends AppCompatActivity {
                 break;
 
             case R.id.btn_sign_up_sign_up:
-                intent = new Intent(SignUpActivity.this, MainActivity.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                finish();
-                break;
+                if (is_suitable_id && is_suitable_pw && is_suitable_pw_again && is_suitable_email && is_suitable_nickname) {
+                    // todo before server connection
+                    // 모든 입력창을 적절하게 채워넣었으므로
+                    // 1. 회원가입이 완료되었다는 알림창 띄우고
+                    // 2. 확인버튼 누르면 SignInActivity로 돌려보내기
+
+                    new AlertDialog.Builder(SignUpActivity.this) // TestActivity 부분에는 현재 Activity의 이름 입력.
+                            .setTitle("Sign Up Completed")
+                            .setMessage("Thank you for joining our community")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                                    startActivity(intent);
+                                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                                    finish();
+                                }
+                            }).setCancelable(false)
+                            .show();
+
+                    // todo after server connection
+                    // 서버 연결 후에는 서버로부터 잘못된 정보 없는지 응답 받은 후에 그에 상응하게 조치하기!!
+                    // ex) 아이디 중복, 닉네임 중복 등 -> 회원정보 수정 요구
+                } else {
+                    Toast.makeText(this, "Please fill out every form properly", Toast.LENGTH_SHORT).show();
+                }
         }
     }
 
