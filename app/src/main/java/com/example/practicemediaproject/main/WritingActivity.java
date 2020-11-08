5dd7ad7b33314690a7ce6971aa3d952e2af9485d
조인성 <insung@joinseong-ui-MacBookPro.local>
2020-11-08 13:01:02 +0900

    20201108 WritingActivity와 레이아웃 추가

---
+++ app/src/main/java/com/example/practicemediaproject/main/WritingActivity.java
@@ -0,0 +1,26 @@
+package com.example.practicemediaproject.main;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.practicemediaproject.R;
+
+
+public class WritingActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing);
+
+
+    }
+}
